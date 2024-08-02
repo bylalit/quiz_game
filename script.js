d8d09ba6ceb8114c494256597f4a6fdecb9bce0d
Lalit0101 <lalitkp0101@gmail.com>
2024-08-02 17:21:57 +0530

    okk

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-// const questions = [
-//   {
-//     question: "Which is larget animal in the world?",
+const questions = [
+  {
+    question: "Which is larget animal in the world?",
 
-//     answers: [
-//       { text: "Shark", correct: false },
-//       { text: "Blue whale", correct: true },
-//       { text: "Elephant", correct: false },
-//       { text: "Giraffe", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Which is the smallest country in the world?",
-//     answers: [
-//       { text: "Vatican City", correct: true },
-//       { text: "Bhutan", correct: false },
-//       { text: "Nepal", correct: false },
-//       { text: "Shri Lanka", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Which is larget desert in the world?",
-//     answers: [
-//       { text: "Kalahari", correct: false },
-//       { text: "Gobi", correct: false },
-//       { text: "Sahara", correct: false },
-//       { text: "Antarctica", correct: true },
-//     ],
-//   },
-//   {
-//     question: "Which is the smallest continent in the world?",
-//     answers: [
-//       { text: "Asia", correct: false },
-//       { text: "Austrialia", correct: true },
-//       { text: "Arctic", correct: false },
-//       { text: "Africa", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Which is larget desert in the world?",
-//     answers: [
-//       { text: "Kalahari", correct: false },
-//       { text: "Gobi", correct: false },
-//       { text: "Sahara", correct: false },
-//       { text: "Antarctica", correct: true },
-//     ],
-//   },
-// ];
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: true },
+      { text: "Bhutan", correct: false },
+      { text: "Nepal", correct: false },
+      { text: "Shri Lanka", correct: false },
+    ],
+  },
+  {
+    question: "Which is larget desert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ],
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      { text: "Asia", correct: false },
+      { text: "Austrialia", correct: true },
+      { text: "Arctic", correct: false },
+      { text: "Africa", correct: false },
+    ],
+  },
+  {
+    question: "Which is larget desert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ],
+  },
+];
 
 
 let qustionElement = document.getElementById("qustion-element");
