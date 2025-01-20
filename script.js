331667131ddaea57aa33b3178af3bef1350a1a29
Lalit0101 <lalitkp0101@gmail.com>
2025-01-20 21:38:19 +0530

    some code error

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ const questions = [
       { text: "Giraffe", correct: false },
     ],
   },
-  {
-    question: "Which is the smallest country in the world?",
-    answers: [
-      { text: "Vatican City", correct: true },
-      { text: "Bhutan", correct: false },
-      { text: "Nepal", correct: false },
-      { text: "Shri Lanka", correct: false },
-    ],
-  },
-  {
-    question: "Which is larget desert in the world?",
-    answers: [
-      { text: "Kalahari", correct: false },
-      { text: "Gobi", correct: false },
-      { text: "Sahara", correct: false },
-      { text: "Antarctica", correct: true },
-    ],
-  },
+  // {
+  //   question: "Which is the smallest country in the world?",
+  //   answers: [
+  //     { text: "Vatican City", correct: true },
+  //     { text: "Bhutan", correct: false },
+  //     { text: "Nepal", correct: false },
+  //     { text: "Shri Lanka", correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "Which is larget desert in the world?",
+  //   answers: [
+  //     { text: "Kalahari", correct: false },
+  //     { text: "Gobi", correct: false },
+  //     { text: "Sahara", correct: false },
+  //     { text: "Antarctica", correct: true },
+  //   ],
+  // },
   {
     question: "Which is the smallest continent in the world?",
     answers: [
